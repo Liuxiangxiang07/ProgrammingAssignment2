d47f6aece6557ebed23cd2d45c241e08245c730d
Xiangxiang Liu <1203741459@qq.com>
2015-06-21 01:33:14 +0800

    add short comments to the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# achieve the function reference to makeVector function in example 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y){
@@ -19,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+# achieve the function reference to chchemean function in example
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getsolve()
